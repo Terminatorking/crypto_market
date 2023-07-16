89fb6dd3413ec18394ba543c777c35232263f751
soheil <soheilghazimoradi@gmail.com>
2023-07-16 09:22:26 +0330

    complete search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,11 +10,60 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    TextEditingController searchController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         title: const Text("CryptoMarket"),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: size.width,
+            child: TextField(
+              controller: searchController,
+              cursorColor: Colors.white,
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.7),
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                hintStyle: TextStyle(
+                  color: Colors.black.withOpacity(0.7),
+                ),
+                fillColor: Colors.greenAccent.withOpacity(0.8),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide(width: 0),
+                ),
+                errorBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide(width: 0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide(width: 0),
+                ),
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide(width: 0),
+                ),
+                hintText: "type your crypto name to search...",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
