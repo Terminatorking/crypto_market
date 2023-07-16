db2b12032e52354f2052a2c8541f3ef72d033468
soheil <soheilghazimoradi@gmail.com>
2023-07-16 09:50:15 +0330

    complete container of crypto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -63,6 +63,78 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: Row(
+                      children: [
+                        Text(
+                          (index + 1).toString(),
+                          style: TextStyle(
+                            color: Colors.grey[300],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "bitcoin",
+                              style: TextStyle(
+                                color: Colors.greenAccent.withOpacity(0.7),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text("BTC"),
+                          ],
+                        ),
+                        const Spacer(),
+                        Column(
+                          children: [
+                            Text(
+                              "bitcoin",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "BTC",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.greenAccent.withOpacity(0.7),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Icon(Icons.trending_up)
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
